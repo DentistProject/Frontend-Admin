09686444f849180b45cc8201e178d606f8b984e3
Nasit Vurgun <nasit@student.chalmers.se>
2023-12-13 07:12:15 +0100

    Integrate Microservices to Frontend Admin
    This commit integrates booking service, user service, and map service in the admin frontend to complete the MVP.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <script setup>
     import Openstreetmap from '../components/Openstreetmap.vue'
+    import NavBar from '../components/NavBar.vue'
 </script>
 
 <template>
     <div>
+        <NavBar />
         <Openstreetmap />
     </div>
 </template>
