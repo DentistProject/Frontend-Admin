2d03c9a13efca0d289929016c215580679d96d9f
Nasit Vurgun <nasit@student.chalmers.se>
2024-01-05 16:01:44 +0100

    Update User Interface

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -9,7 +9,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <router-link to="/booking" class="nav-link">Booking</router-link>
+                        <a href="/booking" class="nav-link">Booking</a>
                     </li>
                 </ul>
                 <span class="navbar-text" v-if="!isUserLoggedIn">
